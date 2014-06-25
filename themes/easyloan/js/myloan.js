7325f6ce1a878d6482d23ffeae206db930963743
songyang <waoywssy@gmail.com>
2014-06-25 11:09:36 +0800

    pagination plug in added
    Signed-off-by: songyang <waoywssy@gmail.com>

--- themes/easyloan/js/myloan.js
+++ themes/easyloan/js/myloan.js
@@ -3,17 +3,6 @@
   Drupal.behaviors.myloan = {
     attach: function(context, settings){
 
-      var max_pages = 50;
-      var per_page = 20;
-
-      var show_pages = 11;
-      var show_pages_mid = 7; // show_pages == (show_pages_mid + 4)
-
-      var total_2 = 0;
-      var total_pages_2 = 0;
-      var total_3 = 0;
-      var total_pages_3 = 0;
-
       var cats = ['','(房产) ','(机车) ','(黄金) ','(信用) ','(其他) '];
 
       $.getJSON( Drupal.settings.basePath + "api/loans", 
@@ -45,10 +34,30 @@
         alert( "获取信息出现问题，请刷新页面。");
       });
 
+      var page_total2 = $('#loan-total-2').html();
+      page_total2 = page_total2 > 0 ? page_total2 : 0;
+      var page_total3 = $('#loan-total-3').html();
+      page_total3 = page_total3 > 0 ? page_total3 : 0;
+      
+      $("#loan-list-pagination-2").pagination({
+        items: page_total2,      // total items 
+        itemsOnPage: 2,  // items per page
+        hrefTextPrefix: '#type=2&page=', 
+        displayedPages:3, 
+      }); 
+
+      $("#loan-list-pagination-3").pagination({
+        items: page_total3,      // total items 
+        itemsOnPage: 2,  // items per page
+        hrefTextPrefix: '#type=3&page=', 
+        displayedPages:3, 
+      });
+
       $(window).bind('hashchange', function(){
         var hash = window.location.hash;
         var type = 2;
         var page = 1;
+
         if (hash.length > 1){
           hash = hash.slice(1);
           var params = hash.split("&");
@@ -79,157 +88,67 @@
 
         $.getJSON( Drupal.settings.basePath + "api/loans?type=" + type + "&page=" + page, 
           function(d) {
-            var list_title = '';
-            var list = '';
-            var pagination = '<ul>';
-            if (page == 1){
-              var total = d.total;
-              var total_pages = total == 0 ? 0 : Math.floor((total - 1) / per_page) + 1;
-
-              if (type == 2){
-                total_2 = total;
-                total_pages_2 = total_pages;
-              }
-              else{
-                total_3 = total;
-                total_pages_3 = total_pages;
-              }
-
-              if (total == 0){
-                list += '<li class="ui-list-status"><p class="color-gray-text">没有记录</p></li>';
-              }
-              else{
-                if (total_pages > max_pages) {
-                  total_pages = max_pages;
-                }
-                if (total_pages == 1){
-                  pagination += '<li class="active"><span class="current">1</span></li>';
-                }
-                else if (total_pages <= show_pages){
-                  pagination += '<li class="active"><span class="current">1</span></li>';
-                  for (var i = 2 ; i <= total_pages; i++){
-                    pagination += '<li><a href="#type=' + type + '&page=' + i + '" class="page-link">' + i + '</a></li>';
-                  }
-                }
-                else{
-                  pagination += '<li class="active"><span class="current prev">前页</span></li><li class="active"><span class="current">1</span></li>';
-                  for (var i = 2 ; i <= (show_pages_mid+2); i++){
-                    pagination += '<li><a href="#type=' + type + '&page=' + i + '" class="page-link">' + i + '</a></li>';
-                  }
-                  pagination += '<li class="disabled"><span class="ellipse">…</span></li><li><a href="#type='
-                    + type + '&page=' + total_pages + '" class="page-link">' + total_pages + '</a></li><li><a href="#type='
-                    + type + '&page=2" class="page-link next">后页</a></li>';
-                }
-              }
+            var total = d.total;
+            if (total > 0){
+              $("#loan-list-pagination-" + type).pagination('updateItems', total); 
+              $('#loan-total-'+type).html(total);
             }
-            else{ // page > 1
-              var total = 0;
-              var total_pages = 0;
-              if (type == 2){
-                total = total_2;
-                total_pages = total_pages_2;
-              }
-              else{
-                total = total_3;
-                total_pages = total_pages_3;
-              }
-              if (total_pages <= show_pages){
-                for (var i = 1; i < page; i++){
-                  pagination += '<li><a href="#type=' + type + '&page=' + i + '" class="page-link">' + i + '</a></li>';
-                }
-                pagination += '<li class="active"><span class="current">' + page + '</span></li>';
-                for (var i = (page+1); i <= total_pages; i++){
-                  pagination += '<li><a href="#type=' + type + '&page=' + i + '" class="page-link">' + i + '</a></li>';
-                }
-              }
-              else{
-                pagination += '<li><a href="#type=' + type + '&page=' + (page-1).toString() + '" class="page-link prev">前页</span></li>';
-                if (page <= show_pages_mid){
-                  for (var i = 1; i < page; i++){
-                    pagination += '<li><a href="#type=' + type + '&page=' + i + '" class="page-link">' + i + '</a></li>';
-                  }
-                  pagination += '<li class="active"><span class="current">' + page + '</span></li>';
-                  for (var i = (page+1); i <= (show_pages_mid+2); i++){
-                    pagination += '<li><a href="#type=' + type + '&page=' + i + '" class="page-link">' + i + '</a></li>';
-                  }
-                  pagination += '<li class="disabled"><span class="ellipse">…</span></li><li><a href="#type='
-                    + type + '&page=' + total_pages + '" class="page-link">' + total_pages + '</a></li><li><a href="#type='
-                    + type + '&page=' + (page+1).toString() + '" class="page-link next">后页</a></li>';
-                }
-                else if (page >= (total_pages - show_pages_mid)){
-                  pagination += '<li><a href="#type=' + type + '&page=1" class="page-link">1</a></li><li class="disabled"><span class="ellipse">…</span></li>';
-                  for (var i = (total_pages - show_pages_mid - 2); i < page; i++){
-                    pagination += '<li><a href="#type=' + type + '&page=' + i + '" class="page-link">' + i + '</a></li>';
-                  }
-                  pagination += '<li class="active"><span class="current">' + page + '</span></li>';
-                  for (var i = (page+1); i <= total_pages; i++){
-                    pagination += '<li><a href="#type=' + type + '&page=' + i + '" class="page-link">' + i + '</a></li>';
-                  }
-                  if (page == total_pages){
-                    pagination += '<li class="active"><span class="current next">后页</span></li>';
-                  }
-                  else{
-                    pagination += '<li><a href="#type='  + type + '&page=' + (page+1).toString() + '" class="page-link next">后页</a></li>';
-                  }
-                }
-                else{// page in the middle
-                  pagination += '<li><a href="#type=' + type + '&page=1" class="page-link">1</a></li><li class="disabled"><span class="ellipse">…</span></li>';
-                  var delta = Math.floor(show_pages_mid / 2.0);
-                  for (var i = (page-delta); i < page; i++){
-                    pagination += '<li><a href="#type=' + type + '&page=' + i + '" class="page-link">' + i + '</a></li>';
-                  }
-                  pagination += '<li class="active"><span class="current">' + page + '</span></li>';
-                  for (var i = (page+1); i <= (page+delta); i++){
-                    pagination += '<li><a href="#type=' + type + '&page=' + i + '" class="page-link">' + i + '</a></li>';
-                  }
-                  pagination += '<li class="disabled"><span class="ellipse">…</span></li><li><a href="#type=' + type + '&page=' + total_pages + '" class="page-link">' + total_pages + '</a></li><li><a href="#type='  + type + '&page=' + (page+1).toString() + '" class="page-link next">后页</a></li>';
-                }
-              }
-            }
-            pagination += '</ul>';
-            $('#loan-list-pagination-'+ type).html(pagination);
-            $('#loan-total-'+type).html("共"+total+"条");
 
+            var list_title = ''; 
+            var list = ''; 
             if (type == 2){ // loans
-              list_title = '<li class="ui-list-header color-gray-text fn-clear"><span class="ui-list-title w220 ph5 fn-left">借款标题</span><span class="ui-list-title w85 ph5 fn-left">借款金额</span><span class="ui-list-title w85 ph5 fn-left">借款利息</span><span class="ui-list-title w55 ph5 fn-left">年利率</span><span class="ui-list-title w30 ph5 fn-left">月数</span><span class="ui-list-title w80 ph5 fn-left">借款日期</span><span class="ui-list-title w80 ph5 fn-left">到期日期</span><span class="ui-list-title w30 fn-left">还清</span></li>';
-              for (var i = 0; i < d.loans.length; i++){
-                var w = d.loans[i];
-                list += '<li class="ui-list-item fn-clear';
-                if (i % 2 == 0){
-                  list += ' dark';
-                }
-                list += '"><span class="ui-list-field w220 ph5 fn-left" style="display: block; overflow: hidden"><a href="/loan_view#id='
-                  + w.id + '" target="blank" title="' + w.title + '">' + cats[w.category] + w.title + '</a></span><span class="ui-list-field w85 ph5 fn-left text-right">' 
-                  + w.amount.toFixed(0).replace(/\B(?=(\d{3})+(?!\d))/g, ",") + '</span><span class="ui-list-field w85 ph5 fn-left text-right">' 
-                  + w.interest.toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ",") + '</span><span class="ui-list-field w55 ph5 fn-left text-right">' 
-                  + (w.rate * 100).toFixed(2) + '%</span><span class="ui-list-field w30 ph5 fn-left text-right">' 
-                  + w.duration.toFixed(0) + '</span><span class="ui-list-field w80 ph5 fn-left text-center">'
-                  + w.start.slice(0, 10) + '</span><span class="ui-list-field w80 ph5 fn-left text-center">'
-                  + w.end.slice(0, 10) + '</span><span class="ui-list-field w30 fn-left text-center">'
-                  + (w.is_done == null ? '' : (w.is_done == 1 ? '是' : '否')) + '</span></li>';
-              }
-              $('#loan-list-2').html(list_title + list);
-            }
-            else{ // loan-application
-              list_title = '<li class="ui-list-header color-gray-text fn-clear"><span class="ui-list-title w300 ph5 fn-left">借款标题</span><span class="ui-list-title w80 ph5 fn-left">计划用款</span><span class="ui-list-title w30 ph5 fn-left">月数</span><span class="ui-list-title w60 ph5 fn-left">申请状态</span><span class="ui-list-title w60 ph5 fn-left">是否放款</span><span class="ui-list-title w60 ph5 fn-left">是否结束</span><span class="ui-list-title w80 ph5 fn-left">申请时间</span></li>';
-              for (var i = 0; i < d.applications.length; i++){
-                var w = d.applications[i];
-                list += '<li class="ui-list-item fn-clear';
-                if (i % 2 == 0){
-                  list += ' dark';
-                }
-                list += '"><span class="ui-list-field w300 ph5 fn-left" style="display: block; overflow: hidden"><a href="/loanapp_view#id='
-                  + w.id + '" target="blank" title="' + w.title + '">' + cats[w.category] + w.title + '</a></span><span class="ui-list-field w80 ph5 fn-left text-right">'
-                  + w.amount.toFixed(0).replace(/\B(?=(\d{3})+(?!\d))/g, ",") + '</span><span class="ui-list-field w30 ph5 fn-left text-right">'
-                  + w.duration.toFixed(0).replace(/\B(?=(\d{3})+(?!\d))/g, ",") + '</span><span class="ui-list-field w60 ph5 fn-left text-center">'
-                  + map_id_name(application_status, w.status) + '</span><span class="ui-list-field w60 ph5 fn-left text-center">'
-                  + (w.is_loaned == null ? '' : (w.is_loaned == 1 ? '是' : '否')) + '</span><span class="ui-list-field w60 ph5 fn-left text-center">'
-                  + (w.is_done == null ? '' : (w.is_done == 1 ? '是' : '否')) + '</span><span class="ui-list-field w80 fn-left text-center">'
-                  + w.applied.slice(0, 10) + '</span></li>';
-              }
-              $('#loan-list-3').html(list_title + list);
-            }
+              var header = $('#loan-list-2').children().get(0);
 
+              if(d.loans.length > 0){
+                $('#loan-list-2').empty().append(header);
+                
+                for (var i = 0; i <= d.loans.length - 1; i++) {
+                  var w = d.loans[i];
+                  var li = $('<li/>').addClass('ui-list-item text fn-clear');
+                  var span = $('<span />').addClass('ui-list-field fn-left');
+                  var row = li.clone()
+                            .append(span.clone().addClass('w220 ph5').css({display: 'block', overflow: 'hidden'})
+                                .append('<a href="' + Drupal.settings.basePath + 'loan_view#id=' + w.id + '" target="blank" title="' + w.title + '">' 
+                                  + cats[w.category] + w.title + '</a>'))
+                            .append(span.clone().addClass('w85 ph5 text-right').append(w.amount.toFixed(0).replace(/\B(?=(\d{3})+(?!\d))/g, ",")))
+                            .append(span.clone().addClass('w85 ph5 text-right').append(w.interest.toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ",")))
+                            .append(span.clone().addClass('w55 ph5 text-right').append((w.rate * 100).toFixed(2)))
+                            .append(span.clone().addClass('w30 ph5 text-right').append(w.duration.toFixed(0)))
+                            .append(span.clone().addClass('w80 ph5 text-center').append(w.start.slice(0, 10)))
+                            .append(span.clone().addClass('w80 ph5 text-center').append(w.end.slice(0, 10)))
+                            .append(span.clone().addClass('w30 text-center').append((w.is_done == null ? '' : (w.is_done == 1 ? '是' : '否'))))
+                  if (i % 2 == 0){
+                    row.addClass('dark');
+                  }
+                  row.appendTo('#loan-list-2');
+                }
+              }
+            } else {
+              var header = $('#loan-list-3').children().get(0);
+              if(d.loans.length > 0){
+                $('#loan-list-3').empty().append(header);
+
+                for (var i = 0; i <= d.applications.length - 1; i++) {
+                  var w = d.applications[i];
+                  var li = $('<li/>').addClass('ui-list-item text fn-clear');
+                  var span = $('<span />').addClass('ui-list-field fn-left ph5');
+                  var row = li.clone()
+                            .append(span.clone().addClass('w300').css({display: 'block', overflow: 'hidden'})
+                                .append('<a href="' + Drupal.settings.basePath + 'loanapp_view#id=' + w.id + '" target="blank" title="' + w.title + '">' 
+                                  + cats[w.category] + w.title + '</a>'))
+                            .append(span.clone().addClass('w80 text-right').append(w.amount.toFixed(0).replace(/\B(?=(\d{3})+(?!\d))/g, ",")))
+                            .append(span.clone().addClass('w30 text-right').append(w.duration.toFixed(0).replace(/\B(?=(\d{3})+(?!\d))/g, ",")))
+                            .append(span.clone().addClass('w60 text-center').append(map_id_name(application_status, w.status)))
+                            .append(span.clone().addClass('w60 text-center').append((w.is_loaned == null ? '' : (w.is_loaned == 1 ? '是' : '否'))))
+                            .append(span.clone().addClass('w60 text-center').append((w.is_done == null ? '' : (w.is_done == 1 ? '是' : '否'))))
+                            .append(span.clone().addClass('w80 text-center').append(w.applied.slice(0, 10)))
+                  if (i % 2 == 0){
+                    row.addClass('dark');
+                  }
+                  row.appendTo('#loan-list-3');
+                }
+              }
+            }
         })
         .fail(function( jqxhr, textStatus, error ) {
           var err = textStatus + ", " + error;
@@ -249,7 +168,6 @@
           window.location.hash = "#type=3";
         }
       });
-
     }
   };
 })(jQuery, Drupal, this, this.document);
--- themes/easyloan/templates/account/myloan.tpl.php
+++ themes/easyloan/templates/account/myloan.tpl.php
@@ -2,15 +2,16 @@
 global $base_url;
 
 $theme_path = drupal_get_path('theme','easyloan');
-
 drupal_add_css($theme_path . '/css/tab.css');
 drupal_add_css($theme_path . '/css/itemlist.css');
 
+drupal_add_js($theme_path . '/js/jquery.simplePagination.js');
 drupal_add_js($theme_path . '/js/application_status.js');
 drupal_add_js($theme_path . '/js/account.js');
 drupal_add_js($theme_path . '/js/tab.js');
 drupal_add_js($theme_path . '/js/utils.js');
 drupal_add_js($theme_path . '/js/myloan.js');
+
 ?>
 <div class="fn-clear p20bs color-orange-bg">
   <div class="fn-left box-summary-left">
@@ -62,15 +63,40 @@ drupal_add_js($theme_path . '/js/myloan.js');
   <div class="p20bs color-white-bg">
     <div class="ui-tab-content fn-clear ui-tab-content-current" data-name="loan">
       <ul class="ui-list ui-list-s" id="loan-list-2">
+        <li class="ui-list-header color-gray-text fn-clear">
+          <span class="ui-list-title w220 ph5 fn-left">借款标题</span>
+          <span class="ui-list-title w85 ph5 fn-left">借款金额</span>
+          <span class="ui-list-title w85 ph5 fn-left">借款利息</span>
+          <span class="ui-list-title w55 ph5 fn-left">年利率</span>
+          <span class="ui-list-title w30 ph5 fn-left">月数</span
+          ><span class="ui-list-title w80 ph5 fn-left">借款日期</span>
+          <span class="ui-list-title w80 ph5 fn-left">到期日期</span>
+          <span class="ui-list-title w30 fn-left">还清</span>
+        </li>
+        <li class="ui-list-status" id="rowempty2">
+          <p class="color-gray-text">没有记录</p>
+        </li>
       </ul>
-      <div class="fn-left mt10" id="loan-total-2"></div>
+      <div class="fn-left mt10">共<span id="loan-total-2">0</span>条</div>
       <div class="fn-right mt10 ui-pagination simple-pagination" id="loan-list-pagination-2">
       </div>
     </div>
     <div class="ui-tab-content fn-clear" data-name="loanapp">
       <ul class="ui-list ui-list-s" id="loan-list-3">
+        <li class="ui-list-header color-gray-text fn-clear">
+          <span class="ui-list-title w300 ph5 fn-left">借款标题</span>
+          <span class="ui-list-title w80 ph5 fn-left">计划用款</span>
+          <span class="ui-list-title w30 ph5 fn-left">月数</span>
+          <span class="ui-list-title w60 ph5 fn-left">申请状态</span>
+          <span class="ui-list-title w60 ph5 fn-left">是否放款</span>
+          <span class="ui-list-title w60 ph5 fn-left">是否结束</span>
+          <span class="ui-list-title w80 ph5 fn-left">申请时间</span>
+        </li>
+        <li class="ui-list-status" id="rowempty3">
+          <p class="color-gray-text">没有记录</p>
+        </li>
       </ul>
-      <div class="fn-left mt10" id="loan-total-3"></div>
+      <div class="fn-left mt10">共<span id="loan-total-3">0</span>条</div>
       <div class="fn-right mt10 ui-pagination simple-pagination" id="loan-list-pagination-3">
       </div>
     </div>
