29115405db36a49c19bda89d4d07214d348d45cc
songyang <waoywssy@gmail.com>
2014-08-20 09:16:47 +0800

    news list block
    Signed-off-by: songyang <waoywssy@gmail.com>

--- themes/hyd/templates/block--easyloan--newslist.tpl.php
+++ themes/hyd/templates/block--easyloan--newslist.tpl.php
@@ -1,7 +1,6 @@
 <?php
-
 global $base_url;
-
+$items = $variables['block']->params;
 ?>
 <!-- News List -->
 <div class="news mb fn-clear" id="news-section">
@@ -10,41 +9,20 @@ global $base_url;
       <span class="on">最新动态</span>
     </div>
     <ul class="ui-list ui-list-m ui-list-news" id="news-list">
+      <?php
+      foreach ($items as $item){
+      ?>
       <li class="ui-list-item fn-clear ">
         <p class="fn-left field decoration"></p>
         <p class="fn-left field text color-silver-text fn-text-overflow">
-          <a class="title text-big rrd-dimgray" href="./about/news/310" target="_blank">好易贷朱祖志参加2013互联网金融年度论坛</a>
+          <a class="title text-big rrd-dimgray" href="./node/<?php print $item['nid']; ?>" target="_blank"><?php print $item['title']; ?></a>
           <span class="content text-small"></span>
         </p>
-        <p class="fn-left field date color-darkgray-text">2014-01-22</p>
+        <p class="fn-left field date color-darkgray-text"><?php print $item['date']; ?></p>
       </li>
-
-      <li class="ui-list-item fn-clear ">
-        <p class="fn-left field decoration"></p>
-        <p class="fn-left field text color-silver-text fn-text-overflow">
-          <a class="title text-big rrd-dimgray" href="./about/news/310" target="_blank">好易贷公司斩获2013中关村十大评选两项大奖</a>
-          <span class="content text-small"></span>
-        </p>
-        <p class="fn-left field date color-darkgray-text">2014-01-16</p>
-      </li>
-
-      <li class="ui-list-item fn-clear ">
-        <p class="fn-left field decoration"></p>
-        <p class="fn-left field text color-silver-text fn-text-overflow">
-          <a class="title text-big rrd-dimgray" href="./about/news/310" target="_blank">好易贷朱祖志受邀出席中国财富管理50人论坛</a>
-          <span class="content text-small"></span>
-        </p>
-        <p class="fn-left field date color-darkgray-text">2014-01-14</p>
-      </li>
-
-      <li class="ui-list-item fn-clear">
-        <p class="fn-left field decoration"></p>
-        <p class="fn-left field text color-silver-text fn-text-overflow">
-          <a class="title text-big rrd-dimgray" href="./about/news/310" target="_blank">好易贷收获亿级美元融资</a>
-          <span class="content text-small"></span>
-        </p>
-        <p class="fn-left field date color-darkgray-text">2014-01-09</p>
-      </li>
+      <?php 
+      } 
+      ?>
       <li class="ui-list-more">
         <a class="darkgray" href="about/news">查看更多网站动态</a>
       </li>
